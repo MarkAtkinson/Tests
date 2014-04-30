25a3881a079b48eb5d029745c038a5a0b7564840
Mark Atkinson <markatkinson00@gmail.com>
2014-04-29 22:18:37 -0400

    commit simple Web API no auth

---
+++ WebAPI/WebAPI/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [MaxLength(50)]
+        public string Description { get; set; }
+    }
+}
